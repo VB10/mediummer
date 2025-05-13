1bf5f624a52ea49b5be36599389f86c1c93ba62c
Veli Bacik <vbacik.10@gmail.com>
2025-05-13 13:14:31 +0300

    Add KeyPathView component to MediummerIOS

---
+++ mediummer/MediummerIOS/MediummerIOS/Feature/KeyPath/KeyPathView.swift
@@ -0,0 +1,18 @@
+//
+//  KeyPathView.swift
+//  MediummerIOS
+//
+//  Created by vb10 on 13.05.2025.
+//
+
+import SwiftUI
+
+struct KeyPathView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KeyPathView()
+}
